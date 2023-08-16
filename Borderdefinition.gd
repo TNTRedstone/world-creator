f5fe4da75103a9a3a6ffaedc1174b31b351917c8
tntredstone <cgsteck@outlook.com>
2023-08-16 09:07:31 -0400

    Optimized borders
    0.0.2

--- Borderdefinition.gd
+++ Borderdefinition.gd
@@ -13,6 +13,34 @@ func calculate_polygon_area(vertices):
 	
 	return abs(area) / 2.0
 
+func filterVector2Array(array: PackedVector2Array) -> PackedVector2Array:
+	var filteredArray: PackedVector2Array = []
+
+	for i in range(array.size()):
+		var isDuplicate = false
+		var isTooClose = false
+		var currentVector = array[i]
+
+		for j in range(filteredArray.size()):
+			var existingVector = filteredArray[j]
+
+			# Check for duplicates
+			if currentVector == existingVector:
+				isDuplicate = true
+				break
+
+			# Check for proximity
+			var distance = currentVector.distance_to(existingVector)
+			if distance < 0.1:
+				isTooClose = true
+				break
+
+		if !isDuplicate and !isTooClose:
+			filteredArray.append(currentVector)
+
+	return filteredArray
+
+
 func _input(_event):
 	if Input.is_mouse_button_pressed(MOUSE_BUTTON_RIGHT):
 		var polygonCopy = polygon + PackedVector2Array([get_local_mouse_position()])
@@ -20,3 +48,4 @@ func _input(_event):
 			firstElement = PackedVector2Array([get_local_mouse_position()])
 		if calculate_polygon_area(polygonCopy) < 100:
 			polygon = polygon + PackedVector2Array([get_local_mouse_position()])
+		polygon = filterVector2Array(polygon)
