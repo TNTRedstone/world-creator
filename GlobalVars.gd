62cc3afa9cd78c4083d75370e2e1710aecdd9725
tntredstone <cgsteck@outlook.com>
2023-08-17 17:15:56 -0400

    Added map arrays
    0.2.0

--- GlobalVars.gd
+++ GlobalVars.gd
@@ -5,6 +5,8 @@ var bordersDefined = false
 var militaryBasesDefined = false
 var redMilitaryBase : Vector2
 var blueMilitaryBase : Vector2
+var blueCoverage
+var redCoverage
 
 func _process(_delta):
 	var window_size = DisplayServer.window_get_size(0)
