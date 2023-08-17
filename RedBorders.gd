5b855e07db6a93ccfa251fddcacc3bcb80cea302
tntredstone <cgsteck@outlook.com>
2023-08-17 17:36:02 -0400

    Revert "Added map arrays"
    This reverts commit 62cc3afa9cd78c4083d75370e2e1710aecdd9725.

--- RedBorders.gd
+++ RedBorders.gd
@@ -30,60 +30,6 @@ func filterVector2Array(array: PackedVector2Array) -> PackedVector2Array:
 		if !isDuplicate and !isTooClose:
 			filteredArray.append(currentVector)
 	return filteredArray
-	
-
-func point_in_polygon(x: float, y: float, PG: Array):
-	var odd_nodes = false
-	var j = PG.size() - 1
-
-	for i in PG.size():
-		if (PG[i].y < y and PG[j].y >= y) or (PG[j].y < y and PG[i].y >= y):
-			if PG[i].x + (y - PG[i].y) / (PG[j].y - PG[i].y) * (PG[j].x - PG[i].x) < x:
-				odd_nodes = !odd_nodes
-		j = i
-
-	return odd_nodes
-
-func create_polygon_array(PG: Array, resolution: float = 0.5):
-	var min_x = get_min_value(PG, "x")
-	var max_x = get_max_value(PG, "x")
-	var min_y = get_min_value(PG, "y")
-	var max_y = get_max_value(PG, "y")
-
-	var width = int((max_x - min_x) / resolution) + 1
-	var height = int((max_y - min_y) / resolution) + 1
-
-	var result = []
-	var y = 0
-	while y < height:
-		result.append([])
-		var x = 0
-		while x < width:
-			var real_x = x * resolution + min_x
-			var real_y = y * resolution + min_y
-			if point_in_polygon(real_x, real_y, PG):
-				result[y].append("#")
-			else:
-				result[y].append(" ")
-			x += 1
-		y += 1
-
-	return result
-
-func get_min_value(array: Array, property: String):
-	var min_value = float("inf")
-	for item in array:
-		if item[property] < min_value:
-			min_value = item[property]
-	return min_value
-
-func get_max_value(array: Array, property: String):
-	var max_value = float("-inf")
-	for item in array:
-		if item[property] > max_value:
-			max_value = item[property]
-	return max_value
-
 func _input(_event):
 	if !GB.bordersDefined:
 		if Input.is_mouse_button_pressed(MOUSE_BUTTON_RIGHT):
@@ -94,4 +40,3 @@ func _input(_event):
 				if calculate_polygon_area(polygonCopy) < 100:
 					polygon = polygon + PackedVector2Array([get_local_mouse_position()])
 				polygon = filterVector2Array(polygon)
-				GB.redCoverage = create_polygon_array(polygon)
